1a0a7ab0bca496c21515441877bc982e3e671375
Luis Rodríguez <lfrz.arc@gmail.com>
2022-04-18 19:01:32 -0500

    Showing data in html

--- logic.js
+++ logic.js
@@ -1,13 +1,19 @@
 const fetchData = async() => {
   try {  
-    const response = await fetch('https://api.themoviedb.org/3/movie/12550/recommendations?api_key=e4b30a1db5bc22a592d00146854380c7');
+    const response = await fetch('https://api.themoviedb.org/3/movie/1550/recommendations?api_key=e4b30a1db5bc22a592d00146854380c7&page=1');
 
     console.log(response);
     if (response.status === 200){
       const data = await response.json();
+
+      let movies = []
+
       data.results.forEach(movie => {
         console.log(movie.title)
-      });
+        movies = movies + `<h3>${movie.title}</h3>`
+      });            
+      document.getElementById('container').innerHTML = movies
+
       console.log(data);
     }else if (response.status == 401){
       console.log('Invalid Key')
