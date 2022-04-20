bf838deb7340de7d733c502192c45c9886476f53
Luis Rodríguez <lfrz.arc@gmail.com>
2022-04-20 17:54:00 -0500

    First presentation of liked and recomendations

--- logic.js
+++ logic.js
@@ -1,20 +1,48 @@
-const fetchData = async() => {
+const getMovie = async() =>{
+  try {
+    const response = await fetch('https://api.themoviedb.org/3/movie/1550?api_key=e4b30a1db5bc22a592d00146854380c7&language=en-US');     
+  
+    if (response.status === 200){      
+      const data = await response.json();
+      console.log(data.title) 
+      let movie = ''
+      movie = `
+        <div class = "poster">
+        <img class="image" src="https://image.tmdb.org/t/p/w500/${data.poster_path}" 
+        </div> 
+        <H1>${data.title}</H1>`
+      document.getElementById('liked').innerHTML = (movie)
+    }else if (response.status == 401){
+      console.log('Invalid Key')
+    }else if (response.status == 404){
+      console.log('This movie doesn\'t exist')
+    }else{
+      console.log('Unexpected error')   
+    }
+  }catch(error){
+    console.log(error)
+  };
+}
+getMovie();
+
+const getRecommendations = async() =>{
   try {  
     const response = await fetch('https://api.themoviedb.org/3/movie/1550/recommendations?api_key=e4b30a1db5bc22a592d00146854380c7&page=1');
-
-    console.log(response);
+  
     if (response.status === 200){
       const data = await response.json();
-
-      let movies = []
-
+  
+      let movies = []  
       data.results.forEach(movie => {
-        console.log(movie.title)
-        movies = movies + `<h3>${movie.title}</h3>`
+        movies += `
+          <div class = "poster">
+            <img class="image" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" 
+          </div> 
+          <h3>${movie.title}</h3>
+        `;
       });            
       document.getElementById('container').innerHTML = movies
-
-      console.log(data);
+  
     }else if (response.status == 401){
       console.log('Invalid Key')
     }else if (response.status == 404){
@@ -25,6 +53,5 @@ const fetchData = async() => {
   }catch(error){
     console.log(error)
   }
-};
-
-fetchData();
+}
+getRecommendations();
