e25399ddae529f00d94c33a7b99f5c1244cf8a89
Luis Rodríguez <lfrz.arc@gmail.com>
2022-04-22 16:57:34 -0500

    Grid almost compleated

--- logic.js
+++ logic.js
@@ -1,14 +1,15 @@
+let search = 315635
 const getMovie = async() =>{
   try {
-    const response = await fetch('https://api.themoviedb.org/3/movie/1550?api_key=e4b30a1db5bc22a592d00146854380c7&language=en-US');     
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${search}?api_key=e4b30a1db5bc22a592d00146854380c7&language=en-US`);     
   
     if (response.status === 200){      
       const data = await response.json();
       console.log(data.title) 
       let movie = ''
       movie = `
-        <img class="image" src="https://image.tmdb.org/t/p/w500/${data.poster_path}" 
-        <H1>${data.title}</H1>`
+        <img class="image" src="https://image.tmdb.org/t/p/w500/${data.poster_path}"/> 
+        <H2>${data.title}</H2>`
       document.getElementById('poster').innerHTML = (movie)
     }else if (response.status == 401){
       console.log('Invalid Key')
@@ -25,16 +26,16 @@ getMovie();
 
 const getRecommendations = async() =>{
   try {  
-    const response = await fetch('https://api.themoviedb.org/3/movie/1550/recommendations?api_key=e4b30a1db5bc22a592d00146854380c7&page=1');
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${search}/recommendations?api_key=e4b30a1db5bc22a592d00146854380c7&page=1`);
   
     if (response.status === 200){
       const data = await response.json();
-  
+      console.log(data)
       let movies = []  
       data.results.forEach(movie => {
         movies += `
-        <div class="recommendations">
-          <img class="container__images" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" 
+        <div class="cards">
+          <img class="cards__images" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
           <h3>${movie.title}</h3>
         </div>
         `;
